bd16832a3a5eaa4f40661abb804d5aa222ce36aa
dmnielsen <dmnielsen@gmail.com>
2021-12-08 23:50:17 -0600

    initial commit

---
+++ aoc2021/util/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+from .print_solutions import print_solutions
+
+
+NEW_DAY_TEMPLATE = Path(__file__).parent.absolute() / 'day_template.txt'
+NEW_TEST_TEMPLATE = Path(__file__).parent.absolute() / 'test_template.txt'
