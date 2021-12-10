f3b99faab833115003d31cc076b443ffe9bc0865
dmnielsen <dmnielsen@gmail.com>
2021-12-10 01:28:42 -0600

    add day one

---
+++ aoc2021/day_01.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from pathlib import Path
+from typing import List
+
+from aoc2021 import AOC_DIR
+from aoc2021.util import print_solutions
+
+
+INPUT_FILENAME = AOC_DIR / 'inputs' / '202101_input.txt'
+
+
+def load(filename: Path = INPUT_FILENAME) -> np.ndarray:
+    with open(filename, 'r') as f:
+        depths = np.loadtxt(f)  # f.read()
+    return depths
+
+
+def solve_part1(input_: np.ndarray) -> int:
+    return sum((input_[1:] - input_[:-1]) > 0)
+
+
+def sum_sliding_windows(depths: np.ndarray, window_size: int) -> np.ndarray:
+    grouped = np.lib.stride_tricks.sliding_window_view(depths, window_size)
+    return np.sum(grouped, 1)
+
+
+def solve_part2(input_: np.ndarray) -> int:
+    depth_windows = sum_sliding_windows(input_, window_size=3)
+    return sum((depth_windows[1:] - depth_windows[:-1]) > 0)
+
+
+def main(show_solution: bool = True):
+    input_ = load(INPUT_FILENAME)
+
+    result1 = solve_part1(input_)
+    result2 = solve_part2(input_)
+
+    if show_solution:
+        print_solutions(result1, result2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_day_01.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+import pytest
+
+from aoc2021 import day_01 as day
+
+
+@pytest.fixture
+def mock_input():
+    depths = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263"""
+    return np.array([int(depth) for depth in depths.split()])
+
+
+def test_solve_part1(mock_input):
+    expected = 7
+    result = day.solve_part1(mock_input)
+    assert expected == result
+
+
+def test_solve_part2(mock_input):
+    expected = 5
+    result = day.solve_part2(mock_input)
+    assert expected == result
