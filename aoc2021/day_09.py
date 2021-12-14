29b5021e982bd2d68dec5b5c9edcca21167f5533
dmnielsen <dmnielsen@gmail.com>
2021-12-13 21:37:19 -0600

    finish day 9

--- aoc2021/day_09.py
+++ aoc2021/day_09.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from math import prod
 from pathlib import Path
 from typing import Dict
 
@@ -7,20 +7,38 @@ from aoc2021.util import print_solutions
 
 INPUT_FILENAME = AOC_DIR / 'inputs' / '202109_input.txt'
 
-Coordinate = namedtuple('Coordinate', ['row', 'col'])
+
+class Coordinate:
+    def __init__(self, row: int, col: int):
+        self.row = row
+        self.col = col
+
+    def __repr__(self):
+        return f'Coordinate(row={self.row}, col={self.col})'
+
+    def __eq__(self, other):
+        return (
+            hasattr(other, 'row')
+            and hasattr(other, 'col')
+            and self.row == other.row
+            and self.col == other.col
+        )
+
+    def __hash__(self):
+        return hash((self.row, self.col))
 
 
 class HeightmapPoint:
-    def __init__(self, coord: Coordinate, value: str, max_row: int, max_col: int):
+    def __init__(self, coord: Coordinate, height: str, max_row: int, max_col: int):
         self.coord = coord
-        self.value = int(value)
-        self.risk = int(value) + 1
+        self.height = int(height)
+        self.risk = int(height) + 1
         self.max_row = max_row
         self.max_col = max_col
         self.neighbors = self.find_neighbors()
 
     def __repr__(self):
-        return f'HeightmapPoint(coord={self.coord}, value={self.value}, max_row={self.max_row}, max_col={self.max_col})'
+        return f'HeightmapPoint(coord={self.coord}, height={self.height}, max_row={self.max_row}, max_col={self.max_col})'
 
     def find_neighbors(self):
         points = [
@@ -43,28 +61,52 @@ def parse_input(text: str) -> Dict[Coordinate, HeightmapPoint]:
     max_row = len(lines)
     max_col = len(lines[0])
 
-    heightmap = {}
+    heightmaps = {}
 
     for row, line in enumerate(lines):
         for col, num in enumerate(line):
             coords = Coordinate(row, col)
             point_info = HeightmapPoint(coords, num, max_row=max_row, max_col=max_col)
-            heightmap[coords] = point_info
-    return heightmap
+            heightmaps[coords] = point_info
+    return heightmaps
 
 
-def solve_part1(input_: str):
+def find_low_points(heightmaps: Dict[Coordinate, HeightmapPoint]):
+    low_points = []
+    for point_coords, info in heightmaps.items():
+        neighbor_values = [heightmaps[coords].height for coords in info.neighbors]
+        if all(info.height < value for value in neighbor_values):
+            low_points.append(point_coords)
+    return low_points
+
+
+def find_basin(low_point: Coordinate, heightmaps: Dict[Coordinate, HeightmapPoint]):
+    basin_points = [low_point]
+    to_check = set(heightmaps[low_point].neighbors)
+    checked = {low_point}
+
+    while to_check:
+        point = to_check.pop()
+        checked.update([point])
+        if heightmaps[point].height < 9:
+            basin_points.append(point)
+            neighbors = heightmaps[point].neighbors
+            to_check.update([n for n in neighbors if n not in checked])
+    return basin_points
+
+
+def solve_part1(input_: str) -> int:
     all_points = parse_input(input_)
-    low_point_risk = []
-    for point_coords, info in all_points.items():
-        neighbor_values = [all_points[coords].value for coords in info.neighbors]
-        if all(info.value < value for value in neighbor_values):
-            low_point_risk.append(info.risk)
-    return sum(low_point_risk)
+    low_points = find_low_points(all_points)
+    return sum([all_points[coords].risk for coords in low_points])
 
 
 def solve_part2(input_):
-    return
+    all_points = parse_input(input_)
+    low_points = find_low_points(all_points)
+    basin_sizes = [len(find_basin(low_point, all_points)) for low_point in low_points]
+
+    return prod(sorted(basin_sizes, reverse=True)[:3])
 
 
 def main(show_solution: bool = True):
--- tests/test_day_09.py
+++ tests/test_day_09.py
@@ -1,6 +1,7 @@
 import pytest
 
 from aoc2021 import day_09 as day
+from aoc2021.day_09 import Coordinate
 
 
 @pytest.fixture
@@ -13,6 +14,49 @@ def mock_input():
     return text
 
 
+@pytest.fixture
+def mock_point_dict(mock_input):
+    return day.parse_input(mock_input)
+
+
+def test_find_low_points(mock_point_dict):
+    expected = [Coordinate(0, 1), Coordinate(0, 9), Coordinate(2, 2), Coordinate(4, 6)]
+    result = day.find_low_points(mock_point_dict)
+    assert len(result) == len(expected)
+    assert all([point in result for point in expected])
+
+
+@pytest.mark.parametrize(
+    'point, expected',
+    [
+        (Coordinate(0, 1), [Coordinate(0, 1), Coordinate(0, 0), Coordinate(1, 0)]),
+        (
+            Coordinate(2, 2),
+            [
+                Coordinate(1, 2),
+                Coordinate(1, 3),
+                Coordinate(1, 4),
+                Coordinate(2, 1),
+                Coordinate(2, 2),
+                Coordinate(2, 3),
+                Coordinate(2, 4),
+                Coordinate(2, 5),
+                Coordinate(3, 0),
+                Coordinate(3, 1),
+                Coordinate(3, 2),
+                Coordinate(3, 3),
+                Coordinate(3, 4),
+                Coordinate(4, 1),
+            ],
+        ),
+    ],
+)
+def test_find_basin(point, expected, mock_point_dict):
+    result = day.find_basin(point, mock_point_dict)
+    assert len(result) == len(expected)
+    assert all([point in result for point in expected])
+
+
 def test_solve_part1(mock_input):
     expected = 15
     result = day.solve_part1(mock_input)
@@ -20,6 +64,6 @@ def test_solve_part1(mock_input):
 
 
 def test_solve_part2(mock_input):
-    expected = None
+    expected = 1134
     result = day.solve_part2(mock_input)
     assert expected == result
