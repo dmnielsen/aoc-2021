6a5a543ce271440b8aeacc90a4227a69cd14372a
dmnielsen <dmnielsen@gmail.com>
2021-12-18 23:53:44 -0600

    finish day 16

--- aoc2021/day_16.py
+++ aoc2021/day_16.py
@@ -1,3 +1,4 @@
+from math import prod
 from pathlib import Path
 from typing import Tuple
 
@@ -36,7 +37,7 @@ def b2int(b: str) -> int:
     return int(b, 2)
 
 
-def part_one_again(i, message) -> Tuple[int, str]:
+def part_one_again(i, message) -> Tuple[int, int]:
     version_sum = b2int(message[i : i + 3])
     type_id = b2int(message[i + 3 : i + 6])
 
@@ -70,13 +71,67 @@ def part_one_again(i, message) -> Tuple[int, str]:
     return i, version_sum
 
 
+OPERATIONS = {
+    0: sum,
+    1: prod,
+    2: min,
+    3: max,
+    5: lambda x: int(x[0] > x[1]),
+    6: lambda x: int(x[0] < x[1]),
+    7: lambda x: int(x[0] == x[1]),
+}
+
+
+def decode_transmission(i, message) -> Tuple[int, int]:
+    version = b2int(message[i : i + 3])
+    type_id = b2int(message[i + 3 : i + 6])
+    # breakpoint()
+
+    i += 6
+
+    if type_id == 4:  # literal type
+        bits = []
+        while True:
+            if message[i] == '0':
+                bits.append(message[i + 1 : i + 5])
+                i += 5
+                break
+            else:
+                bits.append(message[i + 1 : i + 5])
+                i += 5
+        value = b2int(''.join(bits))
+    else:
+        length_type_id = b2int(message[i])
+        i += 1
+        values = []
+        if length_type_id == 0:
+            # next 15 bits define length of sub-packet in bits
+            subpacket_end = i + 15 + b2int(message[i : i + 15])
+            i += 15
+            while i < subpacket_end:
+                i, v = decode_transmission(i, message)
+                values.append(v)
+
+        else:
+            # next 11 bits define count of sub-packets
+            number_of_subpackets = b2int(message[i : i + 11])
+            i += 11
+            for n in range(number_of_subpackets):
+                i, v = decode_transmission(i, message)
+                values.append(v)
+        value = OPERATIONS[type_id](values)
+
+    return i, value
+
+
 def solve_part1(input_: str) -> int:
     message = hex_to_binary(input_)
     return part_one_again(0, message)[1]
 
 
-def solve_part2(input_: str):
-    return
+def solve_part2(input_: str) -> int:
+    message = hex_to_binary(input_)
+    return decode_transmission(0, message)[1]
 
 
 def main(show_solution: bool = True):
--- tests/test_day_16.py
+++ tests/test_day_16.py
@@ -38,7 +38,19 @@ def test_solve_part1(message, expected):
     assert expected == result
 
 
-def test_solve_part2(mock_input):
-    expected = None
-    result = day.solve_part2(mock_input)
+@pytest.mark.parametrize(
+    'message, expected',
+    [
+        ('C200B40A82', 3),
+        ('04005AC33890', 54),
+        ('880086C3E88112', 7),
+        ('CE00C43D881120', 9),
+        ('D8005AC2A8F0', 1),
+        ('F600BC2D8F', 0),
+        ('9C005AC2F8F0', 0),
+        ('9C0141080250320F1802104A08', 1),
+    ],
+)
+def test_solve_part2(message, expected):
+    result = day.solve_part2(message)
     assert expected == result
